09e797a65e034693b5dd2cedd04085bea04e7254
Queli Coto <queli.coto@ticketbis.com>
2016-09-02 10:49:04 +0200

    Organized Kata 18-19-20

--- unicode/17-in-strings/17-unicode-in-strings.js
+++ unicode/17-in-strings/17-unicode-in-strings.js
@@ -1,49 +1,26 @@
-// 1: template strings - basics
-// To do: make all tests pass, leave 	the asserts unchanged!
+// 17: unicode - in strings
+// To do: make all tests pass, leave the assert lines unchanged!
 
-describe('a template string, is wrapped in ` (backticks) instead of \' or "', function() {
+describe('unicode strings', () => {
 
-  describe('by default, behaves like a normal string', function() {
-    
-    it('just surrounded by backticks', function() {
-      var str = `like a string`;
-      assert.equal(str, 'like a string');
-    });
-    
+  it('are \\u prefixed', () => {
+    const nuclear = '\u2622';
+    assert.equal(nuclear, '☢');
   });
 
-  var x = 42;
-  var y = 23;
-  
-  describe('can evaluate variables, which are wrapped in "${" and "}"', function() {
-    
-    it('e.g. a simple variable "${x}" just gets evaluated', function() {
-      var evaluated = `x=${x}`;
-      assert.equal(evaluated, 'x=' + x);
-    });
-    
-    it('multiple variables get evaluated too', function() {
-      var evaluated = `${ x }+${ y }`;
-      assert.equal(evaluated, x + '+' + y);
-    });
-    
+  it('value is 4 bytes/digits', () => {
+    const nuclear = '\u2622';
+    assert.equal(`no more ${nuclear}`, 'no more ☢');
   });
 
-  describe('can evaluate any expression, wrapped inside "${...}"', function() {
-    
-    it('all inside "${...}" gets evaluated', function() {
-      var evaluated = `${ x + y }`;
-      assert.equal(evaluated, x+y);
-    });
-    
-    it('inside "${...}" can also be a function call', function() {
-      function getDomain(){ 
-        return document.domain; 
-      }
-      var evaluated = `${ getDomain() }`;
-      assert.equal(evaluated, 'tddbin.com');
-    });
-    
+  it('value is hexadecimal', () => {
+    const nuclear = `\u006E\u006F more \u2622`;
+    assert.equal(nuclear, 'no more ☢');
   });
-  
+
+  it('curly braces may surround the value', () => {
+    const nuclear = `\u{006E}\u{006F} more \u2622`;
+    assert.equal(nuclear, 'no more ☢');
+  });
+
 });
